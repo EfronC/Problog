5ad036f506fc921b96b3d55bb9804445918dee81
Efraín Cárdenas <Efran Crdenas>
2016-10-24 16:51:11 -0500

    AJAX

--- js/blog.js
+++ js/blog.js
@@ -1,4 +1,30 @@
 /*	javascript de la pagina	*/
+window.onload = function() {
+	var jsonhttp = new XMLHttpRequest(), url="";
+	url = "xml/noticias.json";
+
+	jsonhttp.onreadystatechange = function() {
+		if (jsonhttp.readyState == 4 && jsonhttp.status == 200) {
+			var jsonDoc = JSON.parse(jsonhttp.responseText);
+			rellenar(jsonDoc);
+		}
+	};
+
+	jsonhttp.open("GET",url,true);
+	jsonhttp.send();
+
+	function rellenar(arr) {
+		var out = "";
+		var i;
+		for(i=0;i<arr.length;i++){
+			out += '<article class="nota"><a href="#" class="titulo-nota"><h3>' + arr[i].Titulo + '</h3></a><br>';
+			out += '<div class="fecha-nota">' + arr[i].Fecha + '</div><br>';
+			var nota = arr[i].Nota.substring(0,50);
+			out += '<div class="contenido-nota">' + nota + '...</div></article><br><br>';
+		}
+		document.getElementById("noticias").innerHTML = out;
+	}
+}
 
 function drop(o) {
 	up();
